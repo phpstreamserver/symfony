36e07196d9378b28f81779fc7e37b4a50086ac1b
Anton <anton.z@live.com>
2025-01-13 20:23:27 +0800

    Namespaces refactoring

--- src/Internal/StartCommand.php
+++ src/Internal/StartCommand.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PHPStreamServer\Symfony\Internal;
 
-use PHPStreamServer\Core\Plugin\System\Command\StartCommand as BaseStartCommand;
+use PHPStreamServer\Core\Command\StartCommand as BaseStartCommand;
 
 final class StartCommand extends BaseStartCommand
 {
