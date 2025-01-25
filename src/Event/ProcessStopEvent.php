7247799c9f57bedda26d5017467cd42ab88b65fc
Anton <anton.z@live.com>
2025-01-25 22:34:21 +0800

    Rename HttpServerEvent to ProcessEvent

---
+++ src/Event/ProcessStopEvent.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPStreamServer\Symfony\Event;
+
+use PHPStreamServer\Core\Process;
+use Symfony\Contracts\EventDispatcher\Event;
+
+final class ProcessStopEvent extends Event
+{
+    public function __construct(public readonly Process $worker)
+    {
+    }
+}
