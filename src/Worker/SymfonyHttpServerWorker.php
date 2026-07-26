ce0d8050da7aa223fba7223c0d59b37a94e7b167
Anton <anton.z@live.com>
2026-07-27 01:07:41 +0800

    Add isSerializable method to WorkerInterface

--- src/Worker/SymfonyHttpServerWorker.php
+++ src/Worker/SymfonyHttpServerWorker.php
@@ -55,4 +55,9 @@ final class SymfonyHttpServerWorker extends HttpServerWorker
     {
         return [...parent::handledBy(), SymfonyPlugin::class];
     }
+
+    public function isSerializable(): bool
+    {
+        return false;
+    }
 }
